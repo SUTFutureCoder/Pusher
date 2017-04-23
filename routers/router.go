5b8ac0067155e47f2a32c98034ac873d8fed1e02
linxingchen <linxingchen@baidu.com>
2017-04-23 22:18:14 +0800

    update procfile

--- routers/router.go
+++ routers/router.go
@@ -8,4 +8,7 @@ import (
 func init() {
     beego.Router("/", &controllers.MainController{})
 	beego.Router("/scm/", &controllers.ScmController{}, "get,post:BindScm")
+
+	beego.Router("/oauth/", &controllers.OauthController{}, "get:Oauth")
+	beego.Router("/oauth/echo", &controllers.OauthController{}, "get:OauthEcho")
 }
